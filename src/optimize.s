2084b2e3fabaab1ee820606bca6ff99425b64329
Jefferson111 <e0309385@u.nus.edu>
2019-11-05 14:08:52 +0800

    Eighth commit

--- src/optimize.s
+++ src/optimize.s
@@ -16,13 +16,13 @@ optimize:
  	PUSH {R4-R6}
  	MOV R6, #0 @Initialise counter with 0                           0x03A06000
  	ADD R1, R1 @Set R1 to 2a                                        0x00811001
- 	LDR R5, LAMBDA @Loads lambda into R5
+ 	MOV R5, #10 @Moves lambda (10) into R5                          0x03A0500A
 loop:
  	ADD R6, #1 @Increment counter                                   0x02866001
  	MOV R4, R0 @Store old value of x into R4                        0x01A04000
  	MLA R0, R1, R0, R2 @Calculates Fp = 2ax + b                     0x00220011
  	SDIV R0, R5 @Calculates DELTA = Fp / lambda
- 	ADD R0, R4 @Update value of x                                   0x00800004
+ 	SUB R0, R4, R0 @Update value of x                               0x00440000
  	TEQ R0, R4 @Test equality of old value and new value of x       0x01300004
  	BNE loop @Loop condition                                        0x1800001C
 end:
