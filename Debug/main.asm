792b1f40cbd0fb7beac808d2615b5e5eca1bd243
Jefferson111 <e0309385@u.nus.edu>
2019-11-05 10:45:16 +0800

    Seventh commit

--- Debug/main.asm
+++ Debug/main.asm
@@ -83,21 +83,22 @@
    3:../src/main.c **** // CG2028 Assignment
    4:../src/main.c **** // (C) CK Tham, ECE, NUS
    5:../src/main.c **** 
-   6:../src/main.c **** /** Authors:
-   7:../src/main.c ****  * Alex Teo Kang Jie, A0180338E
-   8:../src/main.c ****  * Chu Jui Hung @Jefferson Chu, A0182590B
-   9:../src/main.c ****  *  Optimization routine written in assembly language
-  10:../src/main.c ****  *  Input parameters (signed integers):
-  11:../src/main.c ****  *     xi : integer version of x
-  12:../src/main.c ****  *        a, b, c: integer coefficients of f(x)
-  13:../src/main.c ****  *        Output : returns solution x (signed integer)
-  14:../src/main.c ****  *        iter: number of iterations
-  15:../src/main.c ****  */
-  16:../src/main.c **** extern int optimize(int xi, int a, int b, int c);
-  17:../src/main.c **** 
-  18:../src/main.c **** int main(void)
-  19:../src/main.c **** {
-  59              		.loc 1 19 0
+   6:../src/main.c **** /**
+   7:../src/main.c ****  * Authors:
+   8:../src/main.c ****  * Alex Teo Kang Jie, A0180338E
+   9:../src/main.c ****  * Chu Jui Hung @Jefferson Chu, A0182590B
+  10:../src/main.c ****  *  Optimization routine written in assembly language
+  11:../src/main.c ****  *  Input parameters (signed integers):
+  12:../src/main.c ****  *     xi : integer version of x
+  13:../src/main.c ****  *        a, b, c: integer coefficients of f(x)
+  14:../src/main.c ****  *        Output : returns solution x (signed integer)
+  15:../src/main.c ****  *        iter: number of iterations
+  16:../src/main.c ****  */
+  17:../src/main.c **** extern int optimize(int xi, int a, int b, int c);
+  18:../src/main.c **** 
+  19:../src/main.c **** int main(void)
+  20:../src/main.c **** {
+  59              		.loc 1 20 0
   60              		.cfi_startproc
   61              		@ args = 0, pretend = 0, frame = 48
   62              		@ frame_needed = 1, uses_anonymous_args = 0
@@ -117,47 +118,47 @@
   76 0006 04AF     		add	r7, sp, #16
   77              	.LCFI2:
   78              		.cfi_def_cfa 7, 80
-  20:../src/main.c ****     int a=10, b=4, c=-3, xsoli;
-  79              		.loc 1 20 0
-  80 0008 4FF00A03 		mov	r3, #10
+  21:../src/main.c ****     int a=3, b=4, c=-3, xsoli;
+  79              		.loc 1 21 0
+  80 0008 4FF00303 		mov	r3, #3
   81 000c FB62     		str	r3, [r7, #44]
   82 000e 4FF00403 		mov	r3, #4
   83 0012 BB62     		str	r3, [r7, #40]
   84 0014 6FF00203 		mvn	r3, #2
   85 0018 7B62     		str	r3, [r7, #36]
-  21:../src/main.c ****     int iter = 0;
-  86              		.loc 1 21 0
+  22:../src/main.c ****     int iter = 0;
+  86              		.loc 1 22 0
   87 001a 4FF00003 		mov	r3, #0
   88 001e BB60     		str	r3, [r7, #8]
-  22:../src/main.c ****     float fp, x, xprev, xsol, change, lambda=0.1;
-  89              		.loc 1 22 0
+  23:../src/main.c ****     float fp, x, xprev, xsol, change, lambda=0.1;
+  89              		.loc 1 23 0
   90 0020 634B     		ldr	r3, .L7	@ float
   91 0022 3B62     		str	r3, [r7, #32]	@ float
-  23:../src/main.c **** 
-  24:../src/main.c ****     printf("Initial value of x (press [Enter] after keying in): "); // try x0 = -6.78
-  92              		.loc 1 24 0
+  24:../src/main.c **** 
+  25:../src/main.c ****     printf("Initial value of x (press [Enter] after keying in): "); // try x0 = -6.78
+  92              		.loc 1 25 0
   93 0024 40F20003 		movw	r3, #:lower16:.LC0
   94 0028 C0F20003 		movt	r3, #:upper16:.LC0
   95 002c 1846     		mov	r0, r3
   96 002e FFF7FEFF 		bl	printf
-  25:../src/main.c ****     scanf("%f", &x);
-  97              		.loc 1 25 0
+  26:../src/main.c ****     scanf("%f", &x);
+  97              		.loc 1 26 0
   98 0032 40F20003 		movw	r3, #:lower16:.LC1
   99 0036 C0F20003 		movt	r3, #:upper16:.LC1
  100 003a 07F10402 		add	r2, r7, #4
  101 003e 1846     		mov	r0, r3
  102 0040 1146     		mov	r1, r2
  103 0042 FFF7FEFF 		bl	scanf
-  26:../src/main.c **** 
-  27:../src/main.c **** //  ARM ASM & Integer version
-  28:../src/main.c ****     printf("ARM ASM & Integer version:\n");
- 104              		.loc 1 28 0
+  27:../src/main.c **** 
+  28:../src/main.c **** //  ARM ASM & Integer version
+  29:../src/main.c ****     printf("ARM ASM & Integer version:\n");
+ 104              		.loc 1 29 0
  105 0046 40F20003 		movw	r3, #:lower16:.LC2
  106 004a C0F20003 		movt	r3, #:upper16:.LC2
  107 004e 1846     		mov	r0, r3
  108 0050 FFF7FEFF 		bl	printf
-  29:../src/main.c ****     xsoli = optimize((int) x * 100, a, 100 * b, &iter); //scaling of 100
- 109              		.loc 1 29 0
+  30:../src/main.c ****     xsoli = optimize((int) x * 100, a, 100 * b, &iter); //scaling of 100
+ 109              		.loc 1 30 0
  110 0054 7B68     		ldr	r3, [r7, #4]	@ float
  111 0056 1846     		mov	r0, r3
  112 0058 FFF7FEFF 		bl	__aeabi_f2iz
@@ -173,8 +174,8 @@
  122 0078 FFF7FEFF 		bl	optimize
  123 007c 0346     		mov	r3, r0
  124 007e FB61     		str	r3, [r7, #28]
-  30:../src/main.c ****     xsol = xsoli / 100.0;
- 125              		.loc 1 30 0
+  31:../src/main.c ****     xsol = xsoli / 100.0;
+ 125              		.loc 1 31 0
  126 0080 F869     		ldr	r0, [r7, #28]
  127 0082 FFF7FEFF 		bl	__aeabi_i2d
  128 0086 0246     		mov	r2, r0
@@ -192,8 +193,8 @@
  140 00a6 FFF7FEFF 		bl	__aeabi_d2f
  141 00aa 0346     		mov	r3, r0
  142 00ac BB61     		str	r3, [r7, #24]	@ float
-  31:../src/main.c ****     printf("xsol : %f \n\n",xsol);
- 143              		.loc 1 31 0
+  32:../src/main.c ****     printf("xsol : %f \n\n",xsol);
+ 143              		.loc 1 32 0
  144 00ae 40F20004 		movw	r4, #:lower16:.LC3
  145 00b2 C0F20004 		movt	r4, #:upper16:.LC3
  146 00b6 B869     		ldr	r0, [r7, #24]	@ float
@@ -202,20 +203,20 @@
  149 00be 0B46     		mov	r3, r1
  150 00c0 2046     		mov	r0, r4
  151 00c2 FFF7FEFF 		bl	printf
-  32:../src/main.c ****     printf("Number of iterations : %d\n\n", iter);
- 152              		.loc 1 32 0
+  33:../src/main.c ****     printf("Number of iterations : %d\n\n", iter);
+ 152              		.loc 1 33 0
  153 00c6 40F20003 		movw	r3, #:lower16:.LC4
  154 00ca C0F20003 		movt	r3, #:upper16:.LC4
  155 00ce BA68     		ldr	r2, [r7, #8]
  156 00d0 1846     		mov	r0, r3
  157 00d2 1146     		mov	r1, r2
  158 00d4 FFF7FEFF 		bl	printf
-  33:../src/main.c **** 
-  34:../src/main.c **** //  NOTE: You DO NOT need to modify the code below this line
-  35:../src/main.c **** 
-  36:../src/main.c **** //  C & Floating Point version
-  37:../src/main.c ****   	printf("C & Floating point version:\n",x);
- 159              		.loc 1 37 0
+  34:../src/main.c **** 
+  35:../src/main.c **** //  NOTE: You DO NOT need to modify the code below this line
+  36:../src/main.c **** 
+  37:../src/main.c **** //  C & Floating Point version
+  38:../src/main.c ****   	printf("C & Floating point version:\n",x);
+ 159              		.loc 1 38 0
  160 00d8 40F20004 		movw	r4, #:lower16:.LC5
  161 00dc C0F20004 		movt	r4, #:upper16:.LC5
  162 00e0 7B68     		ldr	r3, [r7, #4]	@ float
@@ -226,10 +227,10 @@
  167 00ec 2046     		mov	r0, r4
  168 00ee FFF7FEFF 		bl	printf
  169              	.L5:
-  38:../src/main.c ****     while (1)
-  39:../src/main.c ****     {
-  40:../src/main.c ****     	fp = 2*a*x + b;
- 170              		.loc 1 40 0
+  39:../src/main.c ****     while (1)
+  40:../src/main.c ****     {
+  41:../src/main.c ****     	fp = 2*a*x + b;
+ 170              		.loc 1 41 0
  171 00f2 FB6A     		ldr	r3, [r7, #44]
  172 00f4 4FEA4303 		lsl	r3, r3, #1
  173 00f8 1846     		mov	r0, r3
@@ -249,13 +250,13 @@
  187 011a FFF7FEFF 		bl	__aeabi_fadd
  188 011e 0346     		mov	r3, r0
  189 0120 7B61     		str	r3, [r7, #20]	@ float
-  41:../src/main.c **** 
-  42:../src/main.c ****     	xprev = x;
- 190              		.loc 1 42 0
+  42:../src/main.c **** 
+  43:../src/main.c ****     	xprev = x;
+ 190              		.loc 1 43 0
  191 0122 7B68     		ldr	r3, [r7, #4]	@ float
  192 0124 3B61     		str	r3, [r7, #16]	@ float
-  43:../src/main.c ****     	change = -lambda*fp;
- 193              		.loc 1 43 0
+  44:../src/main.c ****     	change = -lambda*fp;
+ 193              		.loc 1 44 0
  194 0126 3B6A     		ldr	r3, [r7, #32]
  195 0128 83F00043 		eor	r3, r3, #-2147483648
  196 012c 1846     		mov	r0, r3
@@ -263,17 +264,17 @@
  198 0130 FFF7FEFF 		bl	__aeabi_fmul
  199 0134 0346     		mov	r3, r0
  200 0136 FB60     		str	r3, [r7, #12]	@ float
-  44:../src/main.c ****     	x += change;
- 201              		.loc 1 44 0
+  45:../src/main.c ****     	x += change;
+ 201              		.loc 1 45 0
  202 0138 7B68     		ldr	r3, [r7, #4]	@ float
  203 013a 1846     		mov	r0, r3
  204 013c F968     		ldr	r1, [r7, #12]	@ float
  205 013e FFF7FEFF 		bl	__aeabi_fadd
  206 0142 0346     		mov	r3, r0
  207 0144 7B60     		str	r3, [r7, #4]	@ float
-  45:../src/main.c **** 
-  46:../src/main.c ****     	printf("x: %f, fp: %f, change: %f\n", x, fp, change);
- 208              		.loc 1 46 0
+  46:../src/main.c **** 
+  47:../src/main.c ****     	printf("x: %f, fp: %f, change: %f\n", x, fp, change);
+ 208              		.loc 1 47 0
  209 0146 40F20004 		movw	r4, #:lower16:.LC6
  210 014a C0F20004 		movt	r4, #:upper16:.LC6
  211 014e 7B68     		ldr	r3, [r7, #4]	@ float
@@ -295,8 +296,8 @@
  227 0178 4246     		mov	r2, r8
  228 017a 4B46     		mov	r3, r9
  229 017c FFF7FEFF 		bl	printf
-  47:../src/main.c ****     	if (x==xprev) break;
- 230              		.loc 1 47 0
+  48:../src/main.c ****     	if (x==xprev) break;
+ 230              		.loc 1 48 0
  231 0180 7B68     		ldr	r3, [r7, #4]	@ float
  232 0182 1846     		mov	r0, r3
  233 0184 3969     		ldr	r1, [r7, #16]	@ float
@@ -304,21 +305,21 @@
  235 018a 0346     		mov	r3, r0
  236 018c 002B     		cmp	r3, #0
  237 018e 00D1     		bne	.L6
-  48:../src/main.c ****     }
- 238              		.loc 1 48 0
+  49:../src/main.c ****     }
+ 238              		.loc 1 49 0
  239 0190 AFE7     		b	.L5
  240              	.L6:
-  47:../src/main.c ****     	if (x==xprev) break;
- 241              		.loc 1 47 0
+  48:../src/main.c ****     	if (x==xprev) break;
+ 241              		.loc 1 48 0
  242 0192 00BF     		nop
  243              	.L4:
-  49:../src/main.c **** 
-  50:../src/main.c ****     // Enter an infinite loop, just incrementing a counter
-  51:../src/main.c **** 	// This is for convenience to allow registers, variables and memory locations to be inspected at t
-  52:../src/main.c **** 	volatile static int loop = 0;
-  53:../src/main.c **** 	while (1) {
-  54:../src/main.c **** 		loop++;
- 244              		.loc 1 54 0 discriminator 1
+  50:../src/main.c **** 
+  51:../src/main.c ****     // Enter an infinite loop, just incrementing a counter
+  52:../src/main.c **** 	// This is for convenience to allow registers, variables and memory locations to be inspected at t
+  53:../src/main.c **** 	volatile static int loop = 0;
+  54:../src/main.c **** 	while (1) {
+  55:../src/main.c **** 		loop++;
+ 244              		.loc 1 55 0 discriminator 1
  245 0194 40F20003 		movw	r3, #:lower16:loop.3827
  246 0198 C0F20003 		movt	r3, #:upper16:loop.3827
  247 019c 1B68     		ldr	r3, [r3, #0]
@@ -326,8 +327,8 @@
  249 01a2 40F20003 		movw	r3, #:lower16:loop.3827
  250 01a6 C0F20003 		movt	r3, #:upper16:loop.3827
  251 01aa 1A60     		str	r2, [r3, #0]
-  55:../src/main.c **** 	}
- 252              		.loc 1 55 0 discriminator 1
+  56:../src/main.c **** 	}
+ 252              		.loc 1 56 0 discriminator 1
  253 01ac F2E7     		b	.L4
  254              	.L8:
  255 01ae 00BF     		.align	2
@@ -343,19 +344,19 @@
  266              	.Letext0:
 DEFINED SYMBOLS
                             *ABS*:00000000 main.c
-C:\Users\jeffe\AppData\Local\Temp\cc7wo8oO.s:19     .rodata:00000000 $d
-C:\Users\jeffe\AppData\Local\Temp\cc7wo8oO.s:20     .rodata:00000000 .LC0
-C:\Users\jeffe\AppData\Local\Temp\cc7wo8oO.s:24     .rodata:00000038 .LC1
-C:\Users\jeffe\AppData\Local\Temp\cc7wo8oO.s:27     .rodata:0000003c .LC2
-C:\Users\jeffe\AppData\Local\Temp\cc7wo8oO.s:34     .rodata:00000058 .LC3
-C:\Users\jeffe\AppData\Local\Temp\cc7wo8oO.s:38     .rodata:00000068 .LC4
-C:\Users\jeffe\AppData\Local\Temp\cc7wo8oO.s:41     .rodata:00000084 .LC5
-C:\Users\jeffe\AppData\Local\Temp\cc7wo8oO.s:47     .rodata:000000a4 .LC6
-C:\Users\jeffe\AppData\Local\Temp\cc7wo8oO.s:51     .text.main:00000000 $t
-C:\Users\jeffe\AppData\Local\Temp\cc7wo8oO.s:56     .text.main:00000000 main
-C:\Users\jeffe\AppData\Local\Temp\cc7wo8oO.s:263    .bss:00000000 loop.3827
-C:\Users\jeffe\AppData\Local\Temp\cc7wo8oO.s:257    .text.main:000001b0 $d
-C:\Users\jeffe\AppData\Local\Temp\cc7wo8oO.s:262    .bss:00000000 $d
+C:\Users\jeffe\AppData\Local\Temp\ccXJU9lC.s:19     .rodata:00000000 $d
+C:\Users\jeffe\AppData\Local\Temp\ccXJU9lC.s:20     .rodata:00000000 .LC0
+C:\Users\jeffe\AppData\Local\Temp\ccXJU9lC.s:24     .rodata:00000038 .LC1
+C:\Users\jeffe\AppData\Local\Temp\ccXJU9lC.s:27     .rodata:0000003c .LC2
+C:\Users\jeffe\AppData\Local\Temp\ccXJU9lC.s:34     .rodata:00000058 .LC3
+C:\Users\jeffe\AppData\Local\Temp\ccXJU9lC.s:38     .rodata:00000068 .LC4
+C:\Users\jeffe\AppData\Local\Temp\ccXJU9lC.s:41     .rodata:00000084 .LC5
+C:\Users\jeffe\AppData\Local\Temp\ccXJU9lC.s:47     .rodata:000000a4 .LC6
+C:\Users\jeffe\AppData\Local\Temp\ccXJU9lC.s:51     .text.main:00000000 $t
+C:\Users\jeffe\AppData\Local\Temp\ccXJU9lC.s:56     .text.main:00000000 main
+C:\Users\jeffe\AppData\Local\Temp\ccXJU9lC.s:263    .bss:00000000 loop.3827
+C:\Users\jeffe\AppData\Local\Temp\ccXJU9lC.s:257    .text.main:000001b0 $d
+C:\Users\jeffe\AppData\Local\Temp\ccXJU9lC.s:262    .bss:00000000 $d
                      .debug_frame:00000010 $d
 
 UNDEFINED SYMBOLS
--- src/optimize.s
+++ src/optimize.s
@@ -13,23 +13,23 @@
 optimize:
 @  Write optimization function in assembly language here
 @  Currently, nothing is done and this function returns straightaway
- 	PUSH {R4-R7}
+ 	PUSH {R4-R6}
  	MOV R6, #0 @Initialise counter with 0                           0x03A06000
  	ADD R1, R1 @Set R1 to 2a                                        0x00811001
  	LDR R5, LAMBDA @Loads lambda into R5
 loop:
  	ADD R6, #1 @Increment counter                                   0x02866001
  	MOV R4, R0 @Store old value of x into R4                        0x01A04000
- 	MLA R7, R1, R0, R2 @Calculates Fp = 2ax + b                     0x00227011
- 	SDIV R7, R5 @Calculates DELTA = Fp / lambda
- 	SUB R0, R7 @Update value of x                                   0x00400007
+ 	MLA R0, R1, R0, R2 @Calculates Fp = 2ax + b                     0x00220011
+ 	SDIV R0, R5 @Calculates DELTA = Fp / lambda
+ 	ADD R0, R4 @Update value of x                                   0x00800004
  	TEQ R0, R4 @Test equality of old value and new value of x       0x01300004
  	BNE loop @Loop condition                                        0x1800001C
 end:
  	STR R6, [R3, #0] @Store the number of iterations                0x05836000
- 	POP {R4-R7}
+ 	POP {R4-R6}
  	BX	LR
 
 @ Define constant values
 CONST: .word 123
-LAMBDA: .word 10
+LAMBDA: .word -10
